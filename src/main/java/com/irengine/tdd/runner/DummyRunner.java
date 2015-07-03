faf004c72bc5434a343a1bd7580739d28e9cd34d
Alex Tang <alex.tang@taoware.com>
2015-07-03 15:08:42 +0800

    add CommandLineRunner

---
+++ src/main/java/com/irengine/tdd/runner/DummyRunner.java
@@ -0,0 +1,19 @@
+package com.irengine.tdd.runner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DummyRunner implements CommandLineRunner {
+
+	private final Logger log = LoggerFactory.getLogger(DummyRunner.class);
+	
+    public void run(String... args) {
+    	
+    	log.info("Dummy Runner");
+
+    }
+
+}
